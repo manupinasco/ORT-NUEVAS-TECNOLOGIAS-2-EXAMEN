7b4d872584f045bd80f14f70b278ec894553ab02
NeverwhereNowhere <manpinas14@gmail.com>
2022-06-09 21:02:58 -0300

    Subo mi resolución del parcial

---
+++ primer_parcial/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container-fluid mt-3">
+    <div class="jumbotron">
+      <h1>Parcial</h1>
+      <br>
+      <Navbar />
+      <router-view></router-view>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+}
+
+h1 {
+  color: rgb(53, 53, 53);
+}
+</style>
